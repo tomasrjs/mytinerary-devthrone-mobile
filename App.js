f2cc003ecea3ab3ff384f55c2382082e55652794
tomasrjs <rojastomas649@gmail.com>
2022-09-26 20:16:03 -0300

    screen added

--- App.js
+++ App.js
@@ -1,16 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './src/screens/HomeScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <HomeScreen></HomeScreen>
   );
 }
 
-const styles = StyleSheet.create({
+/* const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
@@ -18,3 +16,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+ */
