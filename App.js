35bffb4169c98601c44d1e8a00dd6bfbcd1401ca
emazou <estefania.mazou@gmail.com>
2022-09-27 10:49:35 -0500

    store added

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import HomeScreen from './src/screens/HomeScreen';
-
+import {Provider} from 'react-redux'
+import store from './src/features/store'
 export default function App() {
   return (
-    <HomeScreen></HomeScreen>
+    <Provider store={store}>
+      <HomeScreen />
+    </Provider>
   );
 }
 
