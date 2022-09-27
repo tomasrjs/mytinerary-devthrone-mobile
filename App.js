5c4a1c8aeff2aebfd2c5c5eaf99a9fa480bdf4e8
tomasrjs <rojastomas649@gmail.com>
2022-09-27 19:42:02 -0300

    file modified

--- App.js
+++ App.js
@@ -1,22 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import HomeScreen from './src/screens/HomeScreen';
 import {Provider} from 'react-redux'
 import store from './src/features/store'
+import Tabs from './src/navigation/Tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import 'react-native-gesture-handler';
 export default function App() {
   return (
     <Provider store={store}>
-      <HomeScreen />
+      <NavigationContainer>
+        <Tabs/>
+      </NavigationContainer>
     </Provider>
   );
 }
-
-/* const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
- */
