85200064218719e6db655e7851a5bfb1c06d579b
emazou <estefania.mazou@gmail.com>
2022-09-28 14:10:52 -0500

    scrollview added

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,17 @@
-import { ScrollView } from 'react-native';
+import { ScrollView, StyleSheet, Dimensions, View } from 'react-native';
 import Welcome from '../components/Welcome';
 import Carousel from '../components/Carousel';
 
+const width = Dimensions.get("window").width
+const height = Dimensions.get("window").height
+
 export default function HomeScreen() {
   return (
-        <ScrollView>
-            <Welcome />
-            <Carousel />
-        </ScrollView>
+    <View>
+      <ScrollView>
+        <Welcome />
+        <Carousel />
+      </ScrollView>
+    </View>
   )
 }
