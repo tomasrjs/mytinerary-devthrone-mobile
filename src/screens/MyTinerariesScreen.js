474f0facb0ec013ebc087ac08ea9738728e2bf59
tomasrjs <rojastomas649@gmail.com>
2022-09-29 16:13:27 -0300

    import deleted

--- src/screens/MyTinerariesScreen.js
+++ src/screens/MyTinerariesScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Dimensions } from 'react-native'
+import { StyleSheet, View, Dimensions } from 'react-native'
 import React from 'react'
 const width = Dimensions.get("window").width
 const height = Dimensions.get("window").height
@@ -26,7 +26,6 @@ const MyTinerariesScreen = () => {
     React.useEffect(() => {
         getItineraries()
     }, [reload])
-    console.log(user)
     return (
         <ScrollView>
             <View style={styles.container}>
