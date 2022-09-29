120b88e5be9aeb27ccd8e91b28dd158d9b0214a8
emazou <estefania.mazou@gmail.com>
2022-09-28 20:47:00 -0500

    reducer added

--- src/features/loggedSlice.js
+++ src/features/loggedSlice.js
@@ -4,21 +4,26 @@ export const loggedSlice = createSlice({
     name: "logged",
     initialState: {
         loggedState: false,
-        user: null
+        user: null,
+        token: null
     },
     reducers: {
         setUser: (state, action) => {
             state.user = action.payload
-            state.loggedState= true
-            },
+            state.loggedState = true
+        },
         deleteUser: (state, action) => {
             state.user = null
             state.loggedState = false
-            }
+        },
+        setToken: (state, action) => {
+            state.token = action.payload
+            state.loggedState = true
+        },
     }
 
 })
 
-export const { entry, setUser, deleteUser } = loggedSlice.actions
+export const { entry, setUser, deleteUser, setToken } = loggedSlice.actions
 
 export default loggedSlice.reducer
